9ff27a293de3691e1227f7526af0a4a4ec162920
morhsn <mor.hsn@gmail.com>
2014-04-12 16:03:04 +0300

    Version 1.0 of the Tables Creation Script - by Mor.

--- Tables_Creation_Script.sql
+++ Tables_Creation_Script.sql
@@ -1,1 +1,227 @@
-To be completed soon.
+-- phpMyAdmin SQL Dump
+-- version 4.1.8
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Apr 12, 2014 at 04:01 PM
+-- Server version: 5.5.36-cll
+-- PHP Version: 5.4.23
+
+
+--
+-- Version 1.0 of the Tables Creation Script - by Mor.
+--
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `maplehos_bookachoy`
+--
+CREATE DATABASE IF NOT EXISTS `maplehos_bookachoy` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `maplehos_bookachoy`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `book`
+--
+-- Creation: Apr 12, 2014 at 12:58 PM
+--
+
+DROP TABLE IF EXISTS `book`;
+CREATE TABLE IF NOT EXISTS `book` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(120) CHARACTER SET latin1 NOT NULL,
+  `genre` varchar(100) CHARACTER SET latin1 NOT NULL,
+  `overall_rating` tinyint(4) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `name` (`name`),
+  KEY `genre` (`genre`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `loans`
+--
+-- Creation: Apr 12, 2014 at 12:59 PM
+--
+
+DROP TABLE IF EXISTS `loans`;
+CREATE TABLE IF NOT EXISTS `loans` (
+  `user_id` int(11) NOT NULL,
+  `friend_id` int(11) NOT NULL,
+  `book_id` int(11) NOT NULL,
+  `loan_date` date NOT NULL,
+  `due_date` date NOT NULL,
+  `request_date` date NOT NULL,
+  UNIQUE KEY `loan_unique_index` (`user_id`,`friend_id`,`book_id`,`loan_date`),
+  KEY `user_id` (`user_id`),
+  KEY `friend_id` (`friend_id`),
+  KEY `book_id` (`book_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `review`
+--
+-- Creation: Apr 12, 2014 at 01:00 PM
+--
+
+DROP TABLE IF EXISTS `review`;
+CREATE TABLE IF NOT EXISTS `review` (
+  `user_id` int(11) NOT NULL,
+  `book_id` int(11) NOT NULL,
+  `rating` tinyint(4) NOT NULL,
+  `review_text` text CHARACTER SET latin1 NOT NULL,
+  UNIQUE KEY `user_book_ids` (`user_id`,`book_id`),
+  KEY `user_id` (`user_id`),
+  KEY `book_id` (`book_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+-- Creation: Apr 12, 2014 at 01:00 PM
+--
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(60) CHARACTER SET latin1 NOT NULL,
+  `email` varchar(254) CHARACTER SET latin1 NOT NULL,
+  `acct_status` tinyint(4) NOT NULL,
+  `join_date` date NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users_friends`
+--
+-- Creation: Apr 12, 2014 at 01:00 PM
+--
+
+DROP TABLE IF EXISTS `users_friends`;
+CREATE TABLE IF NOT EXISTS `users_friends` (
+  `user_id` int(11) NOT NULL,
+  `friend_id` int(11) NOT NULL,
+  KEY `user_id` (`user_id`,`friend_id`),
+  KEY `friend_id` (`friend_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users_owned_books`
+--
+-- Creation: Apr 12, 2014 at 01:00 PM
+--
+
+DROP TABLE IF EXISTS `users_owned_books`;
+CREATE TABLE IF NOT EXISTS `users_owned_books` (
+  `user_id` int(11) NOT NULL,
+  `book_id` int(11) NOT NULL,
+  `added_date` date NOT NULL,
+  `status` tinyint(4) NOT NULL,
+  KEY `user_id` (`user_id`),
+  KEY `book_id` (`book_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users_read_books`
+--
+-- Creation: Apr 12, 2014 at 01:00 PM
+--
+
+DROP TABLE IF EXISTS `users_read_books`;
+CREATE TABLE IF NOT EXISTS `users_read_books` (
+  `user_id` int(11) NOT NULL,
+  `book_id` int(11) NOT NULL,
+  UNIQUE KEY `user_book_id` (`user_id`,`book_id`),
+  KEY `user_id` (`user_id`),
+  KEY `book_id` (`book_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wishes`
+--
+-- Creation: Apr 12, 2014 at 01:00 PM
+--
+
+DROP TABLE IF EXISTS `wishes`;
+CREATE TABLE IF NOT EXISTS `wishes` (
+  `user_id` int(11) NOT NULL,
+  `book_id` int(11) NOT NULL,
+  `wish_date` date NOT NULL,
+  UNIQUE KEY `user_book_ids` (`user_id`,`book_id`),
+  KEY `user_id` (`user_id`),
+  KEY `book_id` (`book_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `loans`
+--
+ALTER TABLE `loans`
+  ADD CONSTRAINT `loans_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `loans_ibfk_2` FOREIGN KEY (`friend_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `loans_ibfk_3` FOREIGN KEY (`book_id`) REFERENCES `book` (`id`) ON UPDATE CASCADE;
+
+--
+-- Constraints for table `review`
+--
+ALTER TABLE `review`
+  ADD CONSTRAINT `review_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `review_ibfk_2` FOREIGN KEY (`book_id`) REFERENCES `book` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `users_friends`
+--
+ALTER TABLE `users_friends`
+  ADD CONSTRAINT `users_friends_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `users_friends_ibfk_2` FOREIGN KEY (`friend_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `users_owned_books`
+--
+ALTER TABLE `users_owned_books`
+  ADD CONSTRAINT `users_owned_books_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `users_owned_books_ibfk_2` FOREIGN KEY (`book_id`) REFERENCES `book` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `users_read_books`
+--
+ALTER TABLE `users_read_books`
+  ADD CONSTRAINT `users_read_books_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `users_read_books_ibfk_2` FOREIGN KEY (`book_id`) REFERENCES `book` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `wishes`
+--
+ALTER TABLE `wishes`
+  ADD CONSTRAINT `wishes_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `wishes_ibfk_2` FOREIGN KEY (`book_id`) REFERENCES `book` (`id`) ON UPDATE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
