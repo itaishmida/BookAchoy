58fb148c900d8f9df34c0887fafd98457e977247
ShaiFisher <shai.fisher@gmail.com>
2014-05-26 23:50:14 +0300

    fixed friend menu in book page, and also title

--- application/views/book.php
+++ application/views/book.php
@@ -7,21 +7,6 @@
  */
 ?>
 
-<html>
-    <head>
-        <title><?php echo $book["name"]; ?> / <?php echo $book["author"]; ?></title>
-        <link rel="stylesheet" media="screen" href="@routes.Assets.at("stylesheets/main.css")">
-        <link rel="shortcut icon" type="image/png" href="@routes.Assets.at("images/favicon.png")">
-
-        <script src="@routes.Assets.at("javascripts/jquery-1.9.0.min.js")" type="text/javascript"></script>
-        <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
-        <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap-theme.min.css">
-        <script src="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
-
-
-
-    </head>
-    <body>
         <div class="container">
             <div class="row">
                 <div class="col-md-3">
@@ -41,30 +26,10 @@ There was a time in my life when I couldn’t get enough of reading Dostoevsky. 
 There was a time in my life when I couldn’t get enough of reading Dostoevsky. Maybe because his books made me think so deeply about being human and how we choose to live our lives. I began with Crime and Punishment, probably the work he is best known for.
 
 
-
-                    <script>
-                        // Construct URL along with required ISBNs
-                        var isbns = ["0596000278", "00-no-isbn", "ISBN0765304368", "0439554934"];
-                        // Note: server name should be replaced with prod server.
-                        var api_url ="http://books.google.com/books?jscmd=viewapi&bibkeys=" +
-                            isbns.join(",");
-
-                        // Talk to the server synchronously and get _GBSBookInfo object
-                        document.write(unescape("%3Cscript src=" + api_url +
-                            " type='text/javascript'%3E%3C/script%3E"));
-
-                    </script>
-
                 </div>
 
             </div>
         </div>
 
-    </body>
 
-<Script>
-    function getBook(book) {
-        alert(book);
-    }
-</Script>
 </html>
