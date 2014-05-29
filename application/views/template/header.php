64da191dd545d435ee81ac66c46509b9ff018abe
Adi.m <aditrades@gmail.com>
2014-05-29 21:05:35 +0300

    Logo design

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+    <link href='http://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css'>
     <title>BookAchoy <?php echo isset($title) ? " - $title" : ""; ?></title>
     <!-- project's custom CSS -->
     <link rel="stylesheet" href="<?php echo base_url("assets/css/style.css") ?>"/>
@@ -30,7 +30,7 @@
                                 data-target="#bs-example-navbar-collapse-1"><span
                                 class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span
                                 class="icon-bar"></span><span class="icon-bar"></span></button>
-                        <a class="navbar-brand" href="<?php echo base_url() ?>"><?php echo SITE_NAME ?></a>
+                        <a class="navbar-brand logo"   href="<?php echo base_url() ?>"><?php echo SITE_NAME ?></a>
                     </div>
 
                     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
